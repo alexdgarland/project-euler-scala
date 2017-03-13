e09ddd3f9dfa4dfc65b3306e53028eb08d388bdd
Alex Garland <alex.d.garland@gmail.com>
2017-03-13 14:35:06 +0000

    Start moving tests to FunSpec

--- src/test/scala/project_euler/app/SolutionWrapperSpec.scala
+++ src/test/scala/project_euler/app/SolutionWrapperSpec.scala
@@ -1,11 +1,11 @@
 package project_euler.app
 
-import org.scalatest._
+import org.scalatest.{FunSpec, Matchers}
 import org.scalatest.mockito.MockitoSugar
 import org.mockito.Mockito.{when, verify, never}
 import project_euler.solutions.Solution
 
-class SolutionWrapperSpec extends FlatSpec with Matchers with MockitoSugar {
+class SolutionWrapperSpec extends FunSpec with Matchers with MockitoSugar {
 
   private def mockSolution(number : Int, title : String) = {
     val solution = mock[Solution]
@@ -20,32 +20,38 @@ class SolutionWrapperSpec extends FlatSpec with Matchers with MockitoSugar {
 
   private def testWrapper = new SolutionWrapper(mockSolutionList, mock[SolutionRunner])
 
-  behavior of "SolutionWrapper"
+  describe("SolutionWrapper") {
 
-  it should  "have expected toString" in {
-    testWrapper.toString shouldBe "1 - Solve a problem\n2 - Solve another problem"
-  }
+    it("should have expected toString") {
+      testWrapper.toString shouldBe "1 - Solve a problem\n2 - Solve another problem"
+    }
 
-  it should "run full list when runAll is called" in {
-    val wrapper = testWrapper
-    wrapper.runAll()
-    verify(wrapper.runner).run(mockSolutionList)
-  }
+    describe("when running solutions") {
 
-  it should "not run anything when runAll is called against empty list" in {
-    val wrapper = testWrapper
-    wrapper.runAll()
-    verify(testWrapper.runner, never()).run(_)
-  }
+      it("should run full list when runAll is called") {
+        val wrapper = testWrapper
+        wrapper.runAll()
+        verify(wrapper.runner).run(mockSolutionList)
+      }
 
-  it should "run selected solution when runForQuestion is called" in {
-    val wrapper = testWrapper
-    wrapper.runForQuestion(1)
-    verify(wrapper.runner).run(List(mockSolution1))
-  }
+      it("should not run anything when runAll is called against empty list") {
+        val wrapper = testWrapper
+        wrapper.runAll()
+        verify(testWrapper.runner, never()).run(_)
+      }
 
-  it should "throw IllegalArgumentException when runForQuestion called but solution not available" in {
-    intercept[IllegalArgumentException](testWrapper.runForQuestion(3))
+      it("should run selected solution when runForQuestion is called") {
+        val wrapper = testWrapper
+        wrapper.runForQuestion(1)
+        verify(wrapper.runner).run(List(mockSolution1))
+      }
+
+      it("should throw IllegalArgumentException when runForQuestion called but solution not available") {
+        intercept[IllegalArgumentException](testWrapper.runForQuestion(3))
+      }
+
+    }
+
   }
 
 }
