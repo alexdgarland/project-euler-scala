10d90b2ffba06b05b1f5bc9f0ed4e52418663feb
Alex Garland <alex.d.garland@gmail.com>
2017-03-13 20:31:28 +0000

    Make tests a bit more robust in the face of indeterminate ordering

--- src/test/scala/project_euler/solutions/question11/Question11Spec.scala
+++ src/test/scala/project_euler/solutions/question11/Question11Spec.scala
@@ -16,14 +16,21 @@ class Question11Spec extends FunSpec with Matchers {
 
     it("should return expected lines of size 3 against test grid") {
 
-      val expectedResult = IndexedSeq(
-        IndexedSeq(1, 5, 9), IndexedSeq(5, 9, 13),
-        IndexedSeq(2, 6, 10), IndexedSeq(6, 10, 14),
-        IndexedSeq(3, 7, 11), IndexedSeq(7, 11, 15),
-        IndexedSeq(4, 8, 12), IndexedSeq(8, 12, 16)
+      val result = verticalLinesOfSize(3, testGrid)
+
+      result.length shouldBe 8
+
+      result should contain allOf(
+        IndexedSeq(1, 5, 9),
+        IndexedSeq(5, 9, 13),
+        IndexedSeq(2, 6, 10),
+        IndexedSeq(6, 10, 14),
+        IndexedSeq(3, 7, 11),
+        IndexedSeq(7, 11, 15),
+        IndexedSeq(4, 8, 12),
+        IndexedSeq(8, 12, 16)
       )
 
-      verticalLinesOfSize(3, testGrid) shouldBe expectedResult
     }
 
   }
@@ -32,14 +39,21 @@ class Question11Spec extends FunSpec with Matchers {
 
     it("should return expected lines of size 3 against test grid") {
 
-      val expectedResult = IndexedSeq(
-        IndexedSeq(1, 2, 3), IndexedSeq(2, 3, 4),
-        IndexedSeq(5, 6, 7), IndexedSeq(6, 7, 8),
-        IndexedSeq(9, 10, 11), IndexedSeq(10, 11,12),
-        IndexedSeq(13, 14, 15), IndexedSeq(14, 15, 16)
+      val result = horizontalLinesOfSize(3, testGrid)
+
+      result.length shouldBe 8
+
+      result should contain allOf(
+        IndexedSeq(1, 2, 3),
+        IndexedSeq(2, 3, 4),
+        IndexedSeq(5, 6, 7),
+        IndexedSeq(6, 7, 8),
+        IndexedSeq(9, 10, 11),
+        IndexedSeq(10, 11,12),
+        IndexedSeq(13, 14, 15),
+        IndexedSeq(14, 15, 16)
       )
 
-      horizontalLinesOfSize(3, testGrid) shouldBe expectedResult
     }
 
   }
@@ -48,12 +62,17 @@ class Question11Spec extends FunSpec with Matchers {
 
     ignore("should return expected lines of size 3 against test grid") {
 
-      val expectedResult = IndexedSeq(
-        IndexedSeq(9, 6, 3), IndexedSeq(10, 7, 4),
-        IndexedSeq(13, 10, 7), IndexedSeq(14, 11, 8)
+      val result = forwardDiagonalLinesOfSize(3, testGrid)
+
+      result.length shouldBe 4
+
+      result should contain allOf(
+        IndexedSeq(9, 6, 3),
+        IndexedSeq(10, 7, 4),
+        IndexedSeq(13, 10, 7),
+        IndexedSeq(14, 11, 8)
       )
 
-      forwardDiagonalLinesOfSize(3, testGrid) shouldBe expectedResult
     }
 
   }
@@ -62,12 +81,17 @@ class Question11Spec extends FunSpec with Matchers {
 
     ignore("should return expected lines of size 3 against test grid") {
 
-      val expectedResult = IndexedSeq(
-        IndexedSeq(1, 6, 11), IndexedSeq(2, 7, 12),
-        IndexedSeq(5, 10, 15), IndexedSeq(6, 11, 16)
+      val result = backwardDiagonalLinesOfSize(3, testGrid)
+
+      result.length shouldBe 4
+
+      result should contain allOf(
+        IndexedSeq(1, 6, 11),
+        IndexedSeq(2, 7, 12),
+        IndexedSeq(5, 10, 15),
+        IndexedSeq(6, 11, 16)
       )
 
-      backwardDiagonalLinesOfSize(3, testGrid) shouldBe expectedResult
     }
 
   }
